212e6af7e02e32b3520e61f0507aeb6d6cc5178f
Grechkin Danil <grechkin-danil@mail.ru>
2023-05-29 20:53:34 +0300

    fix

--- src/app/api/cards.ts
+++ src/app/api/cards.ts
@@ -29,13 +29,13 @@ export const cards: ICard[] = [
     technology: ['HTML', 'CSS', "Styled-components", 'TypeScript', "NextJS", "Redux", "NestJS","MongoDB", "HTTP",'Axios','Postman', "Figma", "GitHub", "Docker"],
     repo: 'https://github.com/Grekalimbus/TZ-minsk-client' },
 
-    {name: 'Лендинг со слайдерами',
-    image: ["https://i.postimg.cc/3r4byn27/image.png","https://i.postimg.cc/8zDy0G9P/image.png","https://i.postimg.cc/gjFyLqPk/image.png", "https://i.postimg.cc/021Zzx8d/image.png"],
-    title: 'Чего-то большого об этом проекте не скажешь. Небольшой функционал на ванильном JS. Можно посмотреть по ссылке ниже "Демо". Все рисунки сделаны мной лично. В прошлом я занимался тату и хобби рисовать всё еще осталось.',
-    technology: ['HTML', 'CSS', 'JS',"GitHub"],
-    repo: 'https://github.com/Grekalimbus/Sliders-Top-and-Bottom-event-click-button-' ,
-    demo: "https://grekalimbus.github.io/Sliders-Top-and-Bottom-event-click-button-/"}, 
-
+    {name: 'Форма обратной связи',
+    image: ["https://i.postimg.cc/t4BSxPch/image.png","https://i.postimg.cc/CKdvH5y9/image.png","https://i.postimg.cc/sXhwqLgc/image.png"],
+    title: 'Адаптивная форма обратной связи с 3-мя полями. Самое интересно поле с номером, т.к реализована классическая маска. Вводить можно только цифры. Автоматически подставляються скобки где нужно, пробелы, черточки(-). Данные сохраняются в MongoDB, после чего юзера перекидует на страницу, на которой отображаются данные полученные с БД. В демо версии данный функционал отсутствует, но можно склонировать себе репо и убедится в работоспособности клиента с сервером.',
+    technology: ['HTML', 'CSS', 'JS', "React", "Node.js", "MongoDB", "HTTP", "GitHub"],
+    repo: 'https://github.com/Grekalimbus/task-form' ,
+    demo: "https://task-form-htvv-git-main-grekalimbus-s-team.vercel.app/"},  
+    
     {name: 'Игра Сапёр',
     image: ["https://i.postimg.cc/yYdTTX7y/image.png","https://i.postimg.cc/FKtgmM3T/image.png","https://i.postimg.cc/sx1Z5m5B/image.png","https://i.postimg.cc/Nf3rr1FM/image.png"],
     title: 'Игра Сапёр с 3-мя сложностями (10х10/16x16/32x32). Реализовывал таймер. Для каждой сложности время разное. Если время закончилось, игра останавливается и поля некликабельные. Верстка адаптивная. Есть таблица лидеров, в которой данные беруться из редакса. Реализована проверка на то, что юзер выиграл. Если юзер выиграл, поле становится некликабельным и окрашивается в желтый цвет',
@@ -43,19 +43,19 @@ export const cards: ICard[] = [
     repo: 'https://github.com/Grekalimbus/Miner' ,
     demo: "https://miner-git-main-grekalimbus-s-team.vercel.app/"},
 
+    {name: 'Лендинг со слайдерами',
+    image: ["https://i.postimg.cc/3r4byn27/image.png","https://i.postimg.cc/8zDy0G9P/image.png","https://i.postimg.cc/gjFyLqPk/image.png", "https://i.postimg.cc/021Zzx8d/image.png"],
+    title: 'Чего-то большого об этом проекте не скажешь. Небольшой функционал на ванильном JS. Можно посмотреть по ссылке ниже "Демо". Все рисунки сделаны мной лично. В прошлом я занимался тату и хобби рисовать всё еще осталось.',
+    technology: ['HTML', 'CSS', 'JS',"GitHub"],
+    repo: 'https://github.com/Grekalimbus/Sliders-Top-and-Bottom-event-click-button-' ,
+    demo: "https://grekalimbus.github.io/Sliders-Top-and-Bottom-event-click-button-/"},     
+
     {name: 'Aim Game',
     image: ["https://i.postimg.cc/2Sv06MDB/image.png","https://i.postimg.cc/9MxpKjGs/image.png","https://i.postimg.cc/nh8162DV/image.png",'https://i.postimg.cc/HxL0QPfz/image.png'],
     title: 'Мини игра для тренировки Аима. Выбор (10/20/30) сек. После окончания игры, пользователю показывает кол-во попаданий по целям. Есть поле, где рандомно появляются кружочки, на которые нужно кликать.',
     technology: ['HTML', 'CSS', 'JS', "GitHub"],
     repo: 'https://github.com/Grekalimbus/Aim-Game-Task-marathon5' ,
-    demo: "https://grekalimbus.github.io/Aim-Game-Task-marathon5/"},
-
-    {name: 'Форма обратной связи',
-    image: ["https://i.postimg.cc/t4BSxPch/image.png","https://i.postimg.cc/CKdvH5y9/image.png","https://i.postimg.cc/sXhwqLgc/image.png"],
-    title: 'Адаптивная форма обратной связи с 3-мя полями. Самое интересно поле с номером, т.к реализована классическая маска. Вводить можно только цифры. Автоматически подставляються скобки где нужно, пробелы, черточки(-). Данные сохраняются в MongoDB, после чего юзера перекидует на страницу, на которой отображаются данные полученные с БД. В демо версии данный функционал отсутствует, но можно склонировать себе репо и убедится в работоспособности клиента с сервером.',
-    technology: ['HTML', 'CSS', 'JS', "React", "Node.js", "MongoDB", "HTTP", "GitHub"],
-    repo: 'https://github.com/Grekalimbus/task-form' ,
-    demo: "https://task-form-htvv-git-main-grekalimbus-s-team.vercel.app/"},      
+    demo: "https://grekalimbus.github.io/Aim-Game-Task-marathon5/"},      
     
     {name: 'Board-squares',
     image: ["https://i.postimg.cc/yN3ShgjJ/image.png"],
